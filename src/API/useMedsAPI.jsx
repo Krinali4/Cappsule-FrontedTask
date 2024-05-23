0a1890e105c99d7c7d78bce87f14229298d19de3
Krinali4 <paneliyakrinali@gmail.com>
2024-05-23 17:34:42 +0530

    capsule task

--- src/API/useMedsAPI.jsx
+++ src/API/useMedsAPI.jsx
@@ -5,7 +5,7 @@ export const useMedsAPI = async (medicine) => {
     const response = await axios.get(
       `https://backend.cappsule.co.in/api/v1/new_search?q=${medicine}&pharmacyIds=1,2,3`,
     );
-    return response.data;
+    return response.data ;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
